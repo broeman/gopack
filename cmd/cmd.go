d3238e1d68749678f3f3fd6b3dd4a1931f0fa2a8
Jesper Brodersen <brodersendev@gmail.com>
2014-08-28 10:01:49 +0200

    Refactored and implemented CLI command structure

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,1 +1,84 @@
 package cmd
+
+import (
+	"fmt"
+	"github.com/broeman/gopack/pack" // using Package struct
+	"github.com/codegangsta/cli"
+	"os"
+)
+
+var Install = cli.Command{
+	Name:        "install",
+	Usage:       "Installs a package",
+	Description: `Installs a package, that isn't already installed`,
+	Action:      runInstall,
+	Flags:       []cli.Flag{},
+}
+
+var Installed = cli.Command{
+	Name:        "installed",
+	Usage:       "Shows installed packages",
+	Description: `Shows all currently installed packages`,
+	Action:      runInstalled,
+	Flags:       []cli.Flag{},
+}
+
+// placeholders
+var packages = pack.PackageDB()
+
+func runPlaceholder() {
+	dependency := pack.PackageDB()    // empty depencies
+	supversions := pack.NewVersions() // empty versions
+	supversions = append(supversions, pack.NewVersion("0.1b", "SuperTest 0.1 alpha", "alpha", dependency))
+	supversions = append(supversions, pack.NewVersion("1.0", "SuperTest 1.0", "stable", dependency))
+
+	supr := pack.NewPackage("Super", supversions, "[0-9].[0-9][a-z]", true)
+	packages = append(packages, supr)
+
+	dependency = append(dependency, supr) // adding dependency for Test
+	testversions := pack.NewVersions()    // empty versions
+	testversions = append(testversions, pack.NewVersion("0.1a", "TestPackage 0.1a", "alpha", dependency))
+	testversions = append(testversions, pack.NewVersion("0.2", "TestPackage 0.2 Useful", "stable", dependency))
+
+	tester := pack.NewPackage("Test", testversions, "[0-9].[0-9][a-z]", true)
+	packages = append(packages, tester)
+}
+
+func runInstall(ctx *cli.Context) {
+	runPlaceholder()
+	if len(ctx.Args()) != 1 {
+		fmt.Println("You need to specify which package you want to install")
+		os.Exit(2)
+	}
+
+	curpackage := ctx.Args().First()
+	notfound := true
+	for i := range packages {
+		item := packages[i]
+		if curpackage == item.Name() {
+			if item.Installed() {
+				fmt.Println("Package is already installed")
+				notfound = false
+				break
+			} else {
+				item.SetInstalled(true)
+				fmt.Println("Installed package:", curpackage)
+				notfound = false
+				break
+			}
+		}
+	}
+	if notfound {
+		fmt.Println(curpackage, "was not found")
+	}
+}
+
+func runInstalled(*cli.Context) {
+	runPlaceholder()
+	for i := range packages {
+		item := packages[i]
+		if item.Installed() {
+			fmt.Println("Package:", item.Name(), "\nVersion:", item.Version(), "\nDescription:", item.Description(), "\nDependencies:", item.Dependencies(), "\n")
+		}
+	}
+}
--- gopack.go
+++ gopack.go
@@ -7,81 +7,28 @@
 package main
 
 import (
-	. "./pack" // using Package struct
-	"flag"
-	"fmt"
+	"github.com/broeman/gopack/cmd" // using CLI command args
+	"github.com/codegangsta/cli"
 	"os"
+	"runtime"
 )
 
-var packages = PackageDB()
+const APP_VER = "0.1 Alpha"
 
-func main() {
-	dependency := PackageDB()    // empty depencies
-	supversions := NewVersions() // empty versions
-	supversions = append(supversions, NewVersion("0.1b", "SuperTest 0.1 alpha", "alpha", dependency))
-	supversions = append(supversions, NewVersion("1.0", "SuperTest 1.0", "stable", dependency))
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	supr := NewPackage("Super", supversions, "[0-9].[0-9][a-z]", true)
-	packages = append(packages, supr)
-
-	dependency = append(dependency, supr) // adding dependency for Test
-	testversions := NewVersions()         // empty versions
-	testversions = append(testversions, NewVersion("0.1a", "TestPackage 0.1a", "alpha", dependency))
-	testversions = append(testversions, NewVersion("0.2", "TestPackage 0.2 Useful", "stable", dependency))
-
-	tester := NewPackage("Test", testversions, "[0-9].[0-9][a-z]", true)
-	packages = append(packages, tester)
-
-	flag.Usage = usage
-	flag.Parse()
-
-	args := flag.Args()
-	if len(args) > 0 {
-		switch os.Args[1] {
-		case "install":
-			if len(args) > 1 {
-				curpackage := os.Args[2]
-				notfound := true
-				for i := range packages {
-					item := packages[i]
-					if curpackage == item.Name() {
-						if item.Installed() {
-							fmt.Println("Package is already installed")
-							notfound = false
-							break
-						} else {
-							item.SetInstalled(true)
-							fmt.Println("Installed package:", curpackage)
-							notfound = false
-							break
-						}
-					}
-				}
-				if notfound {
-					fmt.Println(curpackage, "was not found")
-				}
-
-			} else {
-				fmt.Println("Please refer which package you want to install")
-			}
-		case "installed":
-			for i := range packages {
-				item := packages[i]
-				if item.Installed() {
-					fmt.Println("Package:", item.Name(), "\nVersion:", item.Version(), "\nDescription:", item.Description(), "\nDependencies:", item.Dependencies(), "\n")
-				}
-			}
-		default:
-		}
-	} else {
-		usage()
-	}
 }
 
-func usage() {
-	fmt.Println("GoPack 0.1: Package Manager in Go")
-	fmt.Println("Usage: gopack <command> <options>")
-	fmt.Println("Commands:")
-	fmt.Println("install - install a package")
-	fmt.Println("installed - show installed packages")
+func main() {
+	app := cli.NewApp()
+	app.Name = "gopack"
+	app.Usage = "Go Package Manager"
+	app.Version = APP_VER
+	app.Commands = []cli.Command{
+		cmd.Install,   // install a package
+		cmd.Installed, // shows current installed packages
+	}
+
+	app.Run(os.Args)
 }
